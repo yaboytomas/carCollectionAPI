f62fd7f1cf1eda8a43f8b06691c76d8ba24a786c
Tomas <tomasszabo94@gmail.com>
2025-08-17 18:40:04 -0400

    first commit, succesfully initialized the MongoDB Atlas connections and .env

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+const connectDB = require('./utils/db');
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`\nServer is running on http://localhost:${PORT}`);
+});
